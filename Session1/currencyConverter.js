c1e99f427787565f462ed4f43df627ad036df90c
Nilton devHack <nilton2005marzo@gmail.com>
2025-03-23 11:07:03 -0500

    add new updates

--- Session1/currencyConverter.js
+++ Session1/currencyConverter.js
@@ -3,14 +3,34 @@ const { stdin: input, stdout: output } = require('node:process');
 
 //const rl = readline.createInterface({ input, output });
 const rl = readline.createInterface({input, output})
-
+//console.log(rl);
 console.log("+++++++++++++++ Convierte de Dorales a Euros ++++++++++++++");
 console.log(
   "Hola usario 🖖🖖, bienvendo al sistemade de conversion de Dolares a Euros"
 )
 
-rl.question("Ingrese en Dolares $ ", (dolares) =>{
-  const fijoEuro = 0.85;
+
+// rl.question("Ingrese en Dolares $ ", (dolares) =>{
+//   const fijoEuro = 0.92;
+//   var multiplo = Math.pow(10, 2)
+//   const inEuros = fijoEuro * dolares;
+//   const inEuros_final = Math.round(inEuros * multiplo) / multiplo;
+//   console.log(`Ingresaste en dolares: ${dolares}
+//                En euros es          : ${inEuros_final}
+//     `);
+//   console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+//   rl.close();
+// })
+rl.setPrompt("Ingrese en $");
+rl.prompt();
+rl.on('line', (dolares)=>{ 
+
+  if(dolares === "salir"){
+    console.log("vuelva pronto");
+    rl.close();
+    return
+  }
+  const fijoEuro = 0.92;
   var multiplo = Math.pow(10, 2)
   const inEuros = fijoEuro * dolares;
   const inEuros_final = Math.round(inEuros * multiplo) / multiplo;
@@ -18,8 +38,14 @@ rl.question("Ingrese en Dolares $ ", (dolares) =>{
                En euros es          : ${inEuros_final}
     `);
   console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-  rl.close();
+  rl.prompt()
+
 })
+  rl.on('close', ()=>{
+    console.log("Programa terminado");
+    process.exit(0);
+  })
+
 
 
 
